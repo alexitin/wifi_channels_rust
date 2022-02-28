9c33823c5881f9a5d59940685cfb7a4c1235d40b
Alex <98160020+alexitin@users.noreply.github.com>
2022-02-28 17:56:45 +0300

    Get list devices with access to ethernet

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
-use pcap::Device;
+use pcap::{Capture, Device, Address};
 
 fn main() {
-    let devices = Device::list().unwrap();
-    for dev in devices {
-        println!("{:?}", dev);
+// Get list all net devices.
+    let devices = Device::list();
+    let mut devices = match devices {
+        Ok(dev) => dev,
+        Err(err) => panic!("Error get list devices: {:?}", err),
+    };
+    println!("List all net devices:");
+    for device in &devices {
+        println!("{:?}", device);
+    }
+
+//Drop devices without access to ethernet,
+//a sign of access to the network is the some value of the broadcast address.
+//Ip addresses collect in Vec<Address>, where first element is values for IPV6 and second element is values for IPV4.
+    devices.retain(|device| -> bool {
+        (device.addresses.len() > 0 && device.addresses[0].broadcast_addr.is_some())
+        ||
+        (device.addresses.len() > 1 && device.addresses[1].broadcast_addr.is_some())
+    });
+    println!("Devices with access to ethernet:");
+    for device in &devices {
+        println!("{:?}", device);
     }
 }
