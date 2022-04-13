748c2db700ae5ccf441eacd4530fde3aa20d98e7
Alex <98160020+alexitin@users.noreply.github.com>
2022-04-13 18:38:50 +0300

    some code restructuring

--- src/lib.rs
+++ src/lib.rs
@@ -1,166 +1,138 @@
-use pcap::{Capture, Device, Linktype};
+use pcap::{Capture, Device, Linktype, Active};
 use std::io;
 use std::process;
 
-pub struct Captured {
-    pub device: Option<Device>,
+pub struct AllDevices {
+    devices: Vec<Device>
+}
+pub struct WifiDevice {
+    pub device: Option<Capture<Active>>,
     pub mode: String,
-    pub linktype: Vec<Linktype>,
 }
 
-impl Captured {
+impl AllDevices {
 
-    pub fn get_monitor_device(devices: Vec<Device>) -> Captured {
-        let device = devices.into_iter()
-            .filter(|dev| Capture::from_device(dev.clone())
-                .unwrap_or_else(|err| panic!("Problem capture device: {}", err))
-                .rfmon(true)
-                .open()
-                .is_ok())
-            .next();
-//        let device: Option<Device> = None;
-        if device.is_some() {
-            let name = device.clone().unwrap().name;
-            Captured {
-                device: device,
-                mode: "monitor".to_string(),
-                linktype: Capture::from_device(name.as_str())
-                    .unwrap()
-                    .rfmon(true)
-                    .open()
-                    .unwrap()
-                    .list_datalinks()
-                    .unwrap_or_else(|err| panic!("Problem capture device: {}", err))
-            }
-        } else {
-            Captured {
-                device: None,
-                mode: "".to_string(),
-                linktype: vec![]
-            }
+    pub fn new () -> Result<AllDevices, pcap::Error> {
+        let devices =  Device::list()?; {
+            Ok(AllDevices {devices})
         }
     }
 
-    pub fn get_promiscuous_device(devices: Vec<Device>) -> Captured {
-        let devices = devices.iter()
-            .cloned()
-            .filter(|dev| (*dev).addresses.len() > 1)
-            .filter(|dev| 
-                ((*dev).addresses[0].broadcast_addr.is_some() ||
-                (*dev).addresses[1].broadcast_addr.is_some()))
-            .collect::<Vec<_>>();
+    pub fn get_wifi_device(self) -> WifiDevice {
 
-        let device = if devices.len() > 1 {
-                let mut i = 0;
-                for dev in &devices {
-                    i += 1;
-                    println!("{}. Device: {} - IPV6: {}, IPV4: {}",
-                        i,
-                        dev.name,
-                        dev.addresses[0].addr,
-                        dev.addresses[1].addr
-                    );
-                }
-                println!("Choose wlan device, or press any key to quit:");
-                let buf = loop {
-                    let mut buf = String::new();
-                    io::stdin()
-                        .read_line(&mut buf)
-                        .expect("Failed read yuor choice:");
-                    let buf = buf.trim().parse::<usize>().unwrap_or(99);
-                    if buf == 99 {
-                        println!("Not found wifi devices");
-                        process::exit(1);
-                    } else if buf > devices.len() {
-                        println!("buf: {}", &buf);
-                        continue;
-                    } else {
-                        break buf
-                    };
-                };
-                Some(devices[buf - 1].clone())
-            } else  if devices.len() == 1 {
-                Some(devices[0].clone())
-            } else {
-                None
-            };
+// Check all devices for monitor mode compatibility and use the first match
+        let device = self.devices.iter()
+            .find_map(|dev| set_monitor_mode(dev).ok());
+//        let device = None;
 
-//        let device: Option<Device> = None;
         if device.is_some() {
-            let name = device.clone().unwrap().name;
-            if Capture::from_device(device.clone().unwrap())
-                .unwrap_or_else(|err| panic!("Problem capture device: {}", err))
-                .promisc(true)
-                .open()
-                .is_ok() {
-                    Captured {
-                        device: device,
-                        mode: "promiscuous".to_string(),
-                        linktype: Capture::from_device(name.as_str())
-                            .unwrap()
-                            .promisc(true)
-                            .open()
-                            .unwrap()
-                            .list_datalinks()
-                            .unwrap_or_else(|err| panic!("Problem capture device: {}", err))
-                    }
-                } else {
-                    Captured {
-                        device: device,
-                        mode: "normal".to_string(),
-                        linktype: Capture::from_device(name.as_str())
-                            .unwrap()
-                            .promisc(true)
-                            .open()
-                            .unwrap()
-                            .list_datalinks()
-                            .unwrap_or_else(|err| panic!("Problem capture device: {}", err))
-                    }
+            WifiDevice {
+                device,
+                mode: "monitor".to_string()
+            }
+        } else {
+
+// Selecting devices connected to the local network
+            let devices = choice_device(self.devices);
+
+// Check device for promiscouos mode and set it
+            let device = set_promiscouos_mode(&devices).ok();
+
+            if device.is_some() {
+                WifiDevice {
+                    device,
+                    mode:"promiscouos".to_string()
                 }
             } else {
-            Captured {
-                device: None,
-                mode: "".to_string(),
-                linktype: vec![]
+// Check device for normal mode and set it
+                let device = set_normal_mode(&devices).ok();
+                WifiDevice {
+                    device,
+                    mode: "normal".to_string()
+                }
             }
         }
     }
+}
 
-    pub fn get_frame(capture_device: Captured) {
-        let mut cap = Capture::from_device(capture_device.device.clone().unwrap())
-            .unwrap()
-            .rfmon(true)
-            .snaplen(128)
-            .buffer_size(256)
-            .timeout(10000)
-            .open()
-            .unwrap();
 
-        if capture_device.linktype.contains(&Linktype(192)) {
-            cap.set_datalink(Linktype::PPI).unwrap()
-        } else if capture_device.linktype.contains(&Linktype(163)) {
-            cap.set_datalink(Linktype::IEEE802_11_AVS).unwrap()
-        } else if capture_device.linktype.contains(&Linktype(127)) {
-            cap.set_datalink(Linktype::IEEE802_11_RADIOTAP).unwrap()
-        } else if capture_device.linktype.contains(&Linktype(119)) {
-            cap.set_datalink(Linktype::IEEE802_11_PRISM).unwrap()
-        } else if capture_device.linktype.contains(&Linktype(105)) {
-            cap.set_datalink(Linktype::IEEE802_11).unwrap()
-        } else if capture_device.linktype.contains(&Linktype(1)) {
-            cap.set_datalink(Linktype::ETHERNET).unwrap()
+fn set_monitor_mode (dev: &Device) -> Result<Capture<Active>, pcap::Error> {
+    Capture::from_device(dev.clone())
+        .unwrap_or_else(|err| panic!("Problem capture device: {}", err))
+        .rfmon(true)
+        .snaplen(128)
+        .buffer_size(256)
+        .timeout(10000)
+        .open()
+}
+
+fn set_promiscouos_mode (dev: &Device) -> Result<Capture<Active>, pcap::Error> {
+    Capture::from_device(dev.clone())
+        .unwrap_or_else(|err| panic!("Problem capture device: {}", err))
+        .promisc(true)
+        .snaplen(128)
+        .buffer_size(256)
+        .timeout(10000)
+        .open()
+}
+
+fn set_normal_mode (dev: &Device) -> Result<Capture<Active>, pcap::Error> {
+    Capture::from_device(dev.clone())
+        .unwrap_or_else(|err| panic!("Problem capture device: {}", err))
+        .snaplen(128)
+        .buffer_size(256)
+        .timeout(10000)
+        .open()
+}
+
+fn choice_device(devices: Vec<Device>) -> Device {
+    let mut i = 0;
+    for dev in &devices {
+        i += 1;
+        match dev.addresses.len() {
+            0 => println!("{}. Device: {} - not connect", &i, &dev.name),
+            1 => println!("{}. Device: {} - IP: {}", i, &dev.name, &dev.addresses[0].addr),
+            _ => println!("{}. Device: {} - IP: {}", i, &dev.name, &dev.addresses[1].addr),
+        }
+    }
+    println!("Choose wlan device, or press any key to quit:");
+    let buf = loop {
+        let mut buf = String::new();
+        io::stdin()
+            .read_line(&mut buf)
+            .unwrap_or_else(|err| {
+                println!("Failed read yuor choice: {}", err);
+                process::exit(1)
+            });
+        let buf = buf.trim().parse::<usize>().unwrap_or_else(|_| {
+            println!("Bye!");
+            process::exit(1)
+        });
+        if buf > devices.len() {
+            println!("Incorrect choice: {}", &buf);
+            continue;
         } else {
-            println!("Not posible capture wifi packets");
-            process::exit(1);
+            break buf
         };
+    };
+    devices[buf - 1].clone()
+}
 
-//        if let Ok(packet) =  cap.next() {
-            println!("Received packets: {:?}", cap.next());
-            println!("Device: {:?},\nMode: {},\nLinktype {:?}",
-                &capture_device.device,
-                &capture_device.mode, 
-                &capture_device.linktype);
-//        }
-
+fn check_linktype(mut device_active: Capture<Active>) -> Result<String, pcap::Error> { 
+    if device_active.set_datalink(Linktype::PPI).is_ok() {
+        Linktype::PPI.get_name()
+    } else if device_active.set_datalink(Linktype::IEEE802_11_AVS).is_ok() {
+        Linktype::IEEE802_11_AVS.get_name()
+    } else if device_active.set_datalink(Linktype::IEEE802_11_RADIOTAP).is_ok() {
+        Linktype::IEEE802_11_RADIOTAP.get_name()
+    } else if device_active.set_datalink(Linktype::IEEE802_11_PRISM).is_ok() {
+        Linktype::IEEE802_11_PRISM.get_name()
+    } else if device_active.set_datalink(Linktype::IEEE802_11).is_ok() {
+        Linktype::IEEE802_11.get_name()
+    } else if device_active.set_datalink(Linktype::ETHERNET).is_ok() {
+        Linktype::ETHERNET.get_name()
+    } else {
+        println!("Not posible capture wifi packets");
+        process::exit(1);
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,28 @@
-use pcap::Device;
 use std::process;
-use wifinsa::Captured;
+use wifinsa::AllDevices;
 
 fn main() {
+
 // Get list all net devices.
-    let devices = match Device::list() {
-        Ok(dev) => dev,
-        Err(err) => panic!("Problem get list devices: {}", err),
-    };
+    let devices = AllDevices::new().unwrap_or_else(|err| {
+        println!("Problem get list all net devices: {}", err);
+        process::exit(1);
+    });
 
 // Get device supporting monitor mode.
-    let mut capture_device = Captured::get_monitor_device(devices.clone());
-
-//If not found wifi devices capable of operating in monitor mode,
-//get device supporting promiscuous mode.
-    if capture_device.device.is_none() {
-    capture_device = Captured::get_promiscuous_device(devices);
-    }
+    let wifi_device = devices.get_wifi_device();
 
 //If not found devices suppoted monitor or promiscuous mode.
-    if capture_device.device.is_none() {
-        println!("Not found wifi devices.
-                Scan of channels not posible.
-NOTE. For promiscuous or normal mode require enable wifi device and connect to wlan.");
+    if wifi_device.device.is_none() {
+        println!("Not found wifi devices. Scan of channels not posible.
+NOTE 1. For promiscuous or normal mode require enable wifi device and connect to wlan.
+NOTE 2. Sometimes superuser rights are needed, try using sudo.");
         process::exit(1);
     } else {
-        Captured::get_frame(capture_device);
-//        println!("Device: {:?},\nMode: {},\nLinktype {:?}",
-//        capture_device.device,
-//        capture_device.mode, 
+//        WifiDevice::get_frame(capture_device);
+        println!("Mode: {},",
+//        wifi_device.device,
+        wifi_device.mode); 
 //        capture_device.linktype);
     }
 }
